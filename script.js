553967ee3b350d19c94c14e903b93d8492d37a87
Naseem780 <mhdnaseem32@gmail.com>
2024-10-10 16:14:37 +0530

    Added Pause and Resume js Function

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const textarea = document.querySelector("textarea");
 const btn = document.querySelector("button");
+let isSpeaking = true;
 const textToSpeech = () =>{
     const synth =  window.speechSynthesis;
     const text = textarea.value;  
-    if (text !== "") {
+    if(text == ""){
+        alert("Enter some text");
+    }
+    if (!synth.speaking && text) {
         const utterance = new SpeechSynthesisUtterance(text);  
         synth.speak(utterance);
-    }else{
-        alert("Enter some text");
-    }  
+        // btn.innerHTML = "Pause";
+        // utterance.onend = () => {
+        //     btn.innerHTML = "Convert to Speech";
+        // };
+    }
+
+    if(text.length > 50){
+        if(synth.speaking && isSpeaking){
+            btn.innerText = "Pause";
+            synth.resume();
+            isSpeaking = false;
+        }else{
+            btn.innerText = "Convert to Speech";
+            synth.pause();
+            isSpeaking = true;
+        }
+    }
 }
-btn.addEventListener("click",textToSpeech);
-    
+btn.addEventListener("click",textToSpeech);   
