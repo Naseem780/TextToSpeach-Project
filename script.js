062389af05ed66735de7aeaa25b50353bd008025
Naseem780 <mhdnaseem32@gmail.com>
2024-10-10 15:39:19 +0530

    Js issue fixed

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const textarea = document.querySelector("textarea");
 const btn = document.querySelector("button");
-
 const textToSpeech = () =>{
     const synth =  window.speechSynthesis;
-    const text = textarea.value;
-
-    
+    const text = textarea.value;  
     if (text !== "") {
         const utterance = new SpeechSynthesisUtterance(text);  
         synth.speak(utterance);
-    }
-    
+    }else{
+        alert("Enter some text");
+    }  
 }
-
 btn.addEventListener("click",textToSpeech);
     
